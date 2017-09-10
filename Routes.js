de601e00ee91f2f9559c84652c5c3ec850eaaf77
apatil65 <arundpatil007@gmail.com>
2017-09-10 17:14:42 +0530

    Update Routes.js

--- Routes.js
+++ Routes.js
@@ -1,7 +1,7 @@
 module.exports = function(app){
     var Fb = require('./controller/controller');
     app.get('/import_data', Fb.import); //imports dummy data for testing
-    //app.get('/food_and_bev', Fb.findAll);
+    app.get('/food_and_bev', Fb.findAll);
     app.get('/food_and_bev/sold', Fb.find);
     app.post('/food_and_bev/add', Fb.add);
     app.put('/update/:type/:item', Fb.update);
